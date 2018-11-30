4201da19d7d68f32e244e73242aa863f35a14ace
S. Hall <shall.usnavy@gmail.com>
2018-11-30 09:17:32 -0500

    Update .gitignore

---
+++ src/com.shall/Oefening.java
@@ -0,0 +1,16 @@
+package com.shall;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // write your code here
+  }
+}

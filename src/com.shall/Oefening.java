35db3834e35aee94e9a07f9348fc7d2bad933dab
S. Hall <shall.usnavy@gmail.com>
2018-11-30 10:20:08 -0500

    Add initial structure for prompting and parsing

--- src/com.shall/Oefening.java
+++ src/com.shall/Oefening.java
@@ -7,10 +7,55 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Scanner;
 
-public class Main {
+public class Oefening {
 
-  public static void main(String[] args) {
-    // write your code here
+  static boolean hadError = false;
+
+  public static void main(String[] args) throws IOException {
+    if (args.length > 1) {
+      System.out.println("Usage: jOefening [script]");
+      System.exit(64);
+    } else if (args.length == 1) {
+      runFile(args[0]);
+    } else {
+      runPrompt();
+    }
+  }
+
+  private static void runFile(String path) throws IOException {
+    byte[] bytes = Files.readAllBytes(Paths.get(path));
+    run(new String(bytes, Charset.defaultCharset()));
+
+    if (hadError) System.exit(65);
+  }
+
+  private static void runPrompt() throws IOException {
+    InputStreamReader input = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(input);
+
+    for (;;) {
+      System.out.println("> ");
+      run(reader.readLine());
+      hadError = false; // A single interactive error shouldn't dump to sys
+    }
+  }
+
+  private static void run(String source) {
+    Scanner scanner = new Scanner(source);
+    List<Token> tokens = scanner.scanTokens();
+
+    for (Token token : tokens) {
+      System.out.println(token);
+    }
+  }
+
+  static void error(int line, String message) {
+    report(line, "", message);
+  }
+
+  private static void report(int line, String where, String message) {
+    System.err.println("[line " + line + "] Error " + where + ": " + message);
   }
 }
